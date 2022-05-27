515f9e7526cbb960b5960ba4fd6473c471931af6
raghu-consultadd <raghunandan.r@consultadd.com>
2022-05-27 14:09:57 +0530

    Added React-Django-Template

---
+++ web_react_django/react_fe/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class ReactFeConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'react_fe'
---
+++ web_react_django/react_fe/src/components/App.js
@@ -0,0 +1,52 @@
+import React, { Component } from "react";
+import { render } from "react-dom";
+
+class New extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: [],
+    };
+  }
+
+  componentDidMount() {
+
+     fetch("events/api")
+
+      .then(response => {
+        return response.json();
+      })
+      .then(data => {
+        this.setState(() => {
+          return {
+            data,
+            loaded: true
+          };
+        });
+      });
+  }
+
+  render() {
+    return (
+     <div>
+        {this.state.data.map(events => {
+          return (
+            <div>
+                Event: {events.title}
+                    <br/>
+                Details: {events.details}
+                <br/>
+                <br/>
+
+            </div>
+          );
+        })}
+      </div>
+    );
+  }
+}
+
+export default New;
+
+const container = document.getElementById("api");
+render(<New />, container);
